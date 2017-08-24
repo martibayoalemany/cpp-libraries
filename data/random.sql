8d0b9ee5574e6343f04cc2ba38666ffd4e46c563
Marti Bayo Alemany <martibayoalemany@gmail.com>
2017-08-25 00:05:03 +0200

    Added random sequel

--- data/random.sql
+++ data/random.sql
@@ -2,7 +2,7 @@ create TABLE input_file (numbers INTEGER)
 .import random_numbers.txt numbers
 .timer on
 select * from input_file LIMIT 10
-select * from input_file order by ASC LIMIT 10
-create index sorting on input_file
-select * from input_file order by ASC LIMIT 10
+select * from input_file order by numbers ASC LIMIT 10
+create index sorting on input_file(numbers)
+select * from input_file order by numbers ASC LIMIT 10
 
